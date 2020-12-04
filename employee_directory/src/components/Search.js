33b4dd1ba6a252ca912a8b86ceff4fd594299d0d
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-12-03 18:45:32 -0600

    create search.js

---
+++ employee_directory/src/components/Search.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/SearchBox.css";
+function Search({ handleSearchChange }) {
+  return (
+    <div className="searchbox">
+      <form className="form-inline">
+        <input
+          className="form-control"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          onChange={e => handleSearchChange(e)}
+        />
+      </form>
+    </div>
+  );
+}
+export default Search;
+
