4f83134ad42d4dd10c2acbb190f5af0430bb86c5
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-12-03 18:36:36 -0600

    add components

---
+++ employee_directory/src/components/Wrapper.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import "../styles/Wrapper.css";
+function Wrapper({ children }) {
+  return (
+      <div className="wrapper">
+        { children }
+      </div>
+  );
+}
+export default Wrapper;
