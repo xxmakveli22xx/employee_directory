3b1a016355f9d1b9cf8c0fb1f9112700118b65d7
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-12-03 18:47:12 -0600

    created Navbar.js

---
+++ employee_directory/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBox from "./SearchBox.js";
+import "../styles/Nav.css";
+function Navbar({ handleSearchChange }) {
+  return (
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <div className="navbar-collapse row" id="navbarNav">
+        <SearchBox handleSearchChange={handleSearchChange} />
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
