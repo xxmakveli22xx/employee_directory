ba5941c07ea28d3837a2ca185b7f46b9cda42211
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-12-05 21:32:46 -0600

    added webpicture

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Wrapper from "./components/Wrapper.js";
 import Main from "./components/Main.js";
 import Header from "./components/Header.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
